f0b5a4f529a2b946136b6bf4a49843bc204ca367
Michael Grote <93448865+michi3214@users.noreply.github.com>
2024-01-23 21:44:26 +0100

    little test for jenkins

--- SOURCE/Web-Application/src/index.ts
+++ SOURCE/Web-Application/src/index.ts
@@ -8,5 +8,5 @@ app.get('/', (req: Request, res: Response) => {
   });
 
   app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}. Please visit this side.`);
   });
